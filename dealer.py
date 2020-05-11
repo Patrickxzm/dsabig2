1e53f2d3382f0057f133f6adda668b49483b6c65
patrickying <spying0403@gmail.com>
2020-05-11 16:52:03 +0800

    Add files via upload
    replace subset with sublist

--- dealer.py
+++ dealer.py
@@ -1,4 +1,5 @@
 import copy
+import collections
 
 class Dealer:
     def __init__(self, v):
@@ -14,11 +15,19 @@ class Dealer:
         self.prec['10'] = 8
         self.prec['J'] = 9
         self.prec['Q'] = 10
-        self.prec['K'] = 11 
+        self.prec['K'] = 11
         self.prec['A'] = 12
         self.prec['2'] = 13
         self.games = ((['A', 'A'], ['2', '3', '3']),
-                (['A', 'A', '9'], ['2', 'Q', 'Q','10', '10', '7','7','5','5','3']))
+                      (['A', 'A', '9'], ['2', 'Q', 'Q','Q', '10', '10', '7', '7', '5', '5', '3']))
+
+    def sublist(self, t):
+        sub = collections.Counter(t)
+        par = collections.Counter(self.cards[self.curr])
+        for k, v in sub.items():
+            if v > par.get(k, 0):
+                return False
+        return True
 
     def deal(self, num):
         self.curr = 0
@@ -27,7 +36,7 @@ class Dealer:
         return copy.deepcopy(self.cards)
 
     def check(self, t):
-        if set(t).issubset(set(self.cards[self.curr])) and self.beat(t, self.t): 
+        if self.sublist(t) and self.beat(t, self.t):
             self.t = t
         else:
             self.t = [] if self.t else [self.cards[self.curr][0]]
@@ -40,54 +49,54 @@ class Dealer:
         return score, self.t
 
     def seq(self, s):
-        if self.prec[s[-1]]-self.prec[s[0]]+1 == len(s):
+        if self.prec[s[-1]] - self.prec[s[0]] + 1 == len(s):
             return (len(s), s[-1])
-        elif self.prec[s[-1]]-self.prec[s[0]] == 12:  # loop
+        elif self.prec[s[-1]] - self.prec[s[0]] == 12:  # loop
             tail = -3 if s[-2] == 'A' else -2
-            if self.prec[s[tail]] == len(s)+tail+1:
+            if self.prec[s[tail]] == len(s) + tail + 1:
                 return (len(s), s[tail])
             else:
                 return None
-    
+
     def pattern(self, t):
         if not t:
             return None
         counter = dict((c, t.count(c)) for c in set(t))
         m = max(counter.values())
-        if m == 1:                                 #单张或顺子
-            s = self.seq(sorted(t, key=lambda x:self.prec[x]))
+        if m == 1:  # 单张或顺子
+            s = self.seq(sorted(t, key=lambda x: self.prec[x]))
             if not s:
                 return None
-            if s[0]==1 or s[0]>4:
+            if s[0] == 1 or s[0] > 4:
                 return ('single*{}'.format(s[0]), s[1])
             else:
                 return None
-        if m == 2:                                #对子或连对
+        if m == 2:  # 对子或连对
             for v in counter.values():
                 if v != 2:
                     return None
-            s = self.seq(sorted(counter.keys(), key=lambda x:self.prec[x]))
+            s = self.seq(sorted(counter.keys(), key=lambda x: self.prec[x]))
             if not s:
                 return None
             return ('pair*{}'.format(s[0]), s[1])
         if m == 3:
-            s = self.seq(sorted([k for k in counter if counter[k]==3],
-                key=lambda x:self.prec[x]))
+            s = self.seq(sorted([k for k in counter if counter[k] == 3],
+                                key=lambda x: self.prec[x]))
             if not s:
                 return None
-            aff = [counter[k] for k in counter if counter[k]<3]
+            aff = [counter[k] for k in counter if counter[k] < 3]
             ptt = ''
             if not aff:
                 ptt = 'triple*{}'
-            elif aff == [1]*s[0]:
+            elif aff == [1] * s[0]:
                 ptt = 'triple*{}+single'
-            elif aff == [2]*s[0]:
+            elif aff == [2] * s[0]:
                 ptt = 'triple*{}+pair'
             else:
                 return None
             return (ptt.format(s[0]), s[1])
         if m == 4:
-            if len(counter) > 1:                   #没有连炸
+            if len(counter) > 1:  # 没有连炸
                 return None
             return ('bomb', t[0])
         return None
@@ -97,9 +106,9 @@ class Dealer:
         yt = self.pattern(yt)
         if not t:
             return False
-        if not yt: 
+        if not yt:
             return True
-        if t[0] == 'bomb' and yt[0] != 'bomb':     #炸弹盖过其他牌型
+        if t[0] == 'bomb' and yt[0] != 'bomb':  # 炸弹盖过其他牌型
             return True
-                                                      #牌型匹配且大于
+            # 牌型匹配且大于
         return t[0] == yt[0] and self.prec[t[1]] > self.prec[yt[1]]
